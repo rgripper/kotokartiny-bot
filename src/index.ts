de610d14fef4437da1aba9e4cc6b22add9f46893
Vladimir Zotov <azuresplash@gmail.com>
2018-03-19 15:11:37 +1100

    Added axios

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,53 @@ import { Strategy as VKontakteStrategy } from 'passport-vkontakte';
 import * as cookieParser from 'cookie-parser';
 import { urlencoded } from 'body-parser';
 import * as passport from 'passport';
+import * as https from 'https';
+
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(response => {
+    console.log(response.data.url);
+    console.log(response.data.explanation);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+ 
+https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  let data = '';
+ 
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+ 
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+ 
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
 
 const app = express()
 // User session support middlewares. Your exact suite might vary depending on your app's needs.
 app.use(cookieParser());
 app.use(urlencoded({extended: true}));
 app.use(require('express-session')({secret:'keyboard cat', resave: true, saveUninitialized: true}));
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.use(new VKontakteStrategy(
-  {
-    clientID:     VKONTAKTE_APP_ID, // VK.com docs call it 'API ID', 'app_id', 'api_id', 'client_id' or 'apiId'
-    clientSecret: VKONTAKTE_APP_SECRET,
-    callbackURL:  "http://localhost:3000/auth/vkontakte/callback"
-  },
-  function myVerifyCallbackFn(accessToken, refreshToken, params, profile, done) {
+https://api.vk.com/method/users.get?user_ids=210700286&fields=bdate&v=5.73
 
-    // Now that we have user's `profile` as seen by VK, we can
-    // use it to find corresponding database records on our side.
-    // Also we have user's `params` that contains email address (if set in 
-    // scope), token lifetime, etc.
-    // Here, we have a hypothetical `User` class which does what it says.
-    User.findOrCreate({ vkontakteId: profile.id })
-        .then(function (user) { done(null, user); })
-        .catch(done);
-  }
-));
+app.get('/auth/vkontakte', passport.authenticate('vkontakte'));
 
-// User session support for our hypothetical `user` objects.
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
+app.get('/auth/vkontakte/callback',
+  passport.authenticate('vkontakte', {
+    successRedirect: '/',
+    failureRedirect: '/login' 
+  })
+);
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id)
-        .then(function (user) { done(null, user); })
-        .catch(done);
+app.get('/', function(req, res) {
+    //Here you have an access to req.user
+    res.json(req.user);
 });
